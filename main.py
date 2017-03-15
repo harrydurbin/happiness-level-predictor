0e9e09620d09f2374be979c8d832ab53f9313651
Harry Durbin <harry.durbin@gmail.com>
2017-03-15 04:12:12 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+from happiness import Happiness
+
+
+
+Happiness().getSurveyData()
+
+Happiness().getCoords()
+
+Happiness().clusterCountries()
+
+Happiness().concatRawData()
+
+Happiness().cleanData()
+
+Happiness().extractFeatures():
+
+Happiness().splitData()
+
+Happiness().createModel('rf')
+
+Happiness().makePredictions()
+
+Happiness.getCV()
