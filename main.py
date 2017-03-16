10c175cf713f2cda8aba76664c9b7a27b98fb831
Harry Durbin <harry.durbin@gmail.com>
2017-03-15 21:16:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 
 from happiness import Happiness
 
-
-
-Happiness().getSurveyData()
-
-Happiness().getCoords()
-
-Happiness().clusterCountries()
-
-Happiness().concatRawData()
-
-Happiness().cleanData()
-
-Happiness().extractFeatures():
-
-Happiness().splitData()
-
-Happiness().createModel('rf')
-
-Happiness().makePredictions()
-
-Happiness.getCV()
+Happiness().getSurveyData() # loads the raw survey data
+Happiness().getCoords() # matches up the country centroid lat and long
+Happiness().clusterCountries() # clusters countries that are near each other
+Happiness().concatRawData() # joins the cluster labels to the raw survey data
+Happiness().cleanData() # cleans errant survey responses
+Happiness().extractFeatures(): # extracts key features from the raw data
+Happiness().splitData() # splits data into train and test sets
+Happiness().createModel('rf') # creates a model to the train data
+Happiness().makePredictions() # predicts target happiness level using model on test data
+Happiness.getCV() # cross validates predictions with actual happiness levels on test data
